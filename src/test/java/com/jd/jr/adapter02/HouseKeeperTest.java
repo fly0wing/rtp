dd8ed2fecedc63483b7d54213f48ef7cd337fa8c
zhangkai74 <zhangkai9@jd.com>
2018-01-23 20:31:51 +0800

    3.添加测试“主人到家，开灯”

---
+++ src/test/java/com/jd/jr/adapter02/HouseKeeperTest.java
@@ -0,0 +1,20 @@
+package com.jd.jr.adapter02;
+
+
+import com.jd.jr.adapter02.equipment.Light;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HouseKeeperTest {
+    @Test
+    public void should_turn_on_light_when_master_arrive_at_home() throws Exception {
+        Light light = new Light();
+        HouseKeeper houseKeeper = new HouseKeeper();
+        houseKeeper.addLight(light);
+        houseKeeper.masterArriveAtHome();
+
+        assertTrue(light.isOn());
+    }
+
+}
