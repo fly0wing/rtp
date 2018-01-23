cd3e981d9516fc1e4ca11c2aba67aa4ac672a47d
zhangkai74 <zhangkai9@jd.com>
2018-01-23 19:36:56 +0800

    3.根据TeatCase生成代码。

---
+++ src/main/java/com/jd/jr/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package com.jd.jr.fizzbuzz;
+
+/**
+ * 需求：
+ * 写一个程序输入一个数值，返回对应的结果。
+ * 1. 但是遇到数字为3的倍数的时候，打印“Fizz”替代数字，
+ * 2. 5的倍数用“Buzz”代替，
+ * 3. 既是3的倍数又是5的倍数打印“FizzBuzz”。
+ *
+ * @author zhangkai9@jd.com
+ * @date 18-1-23
+ */
+public class FizzBuzz {
+    public String process(int i) {
+        return null;
+    }
+}
