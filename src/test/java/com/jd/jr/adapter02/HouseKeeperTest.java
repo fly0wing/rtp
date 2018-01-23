8cf8b4f33a7fb739d50c9d86e3b4ae5a133aa370
zhangkai74 <zhangkai9@jd.com>
2018-01-23 20:36:00 +0800

    5.编写测试TestCase：“主人离家，关灯”

--- src/test/java/com/jd/jr/adapter02/HouseKeeperTest.java
+++ src/test/java/com/jd/jr/adapter02/HouseKeeperTest.java
@@ -5,6 +5,7 @@ import com.jd.jr.adapter02.HouseKeeper;
 import com.jd.jr.adapter02.equipment.Light;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class HouseKeeperTest {
@@ -16,6 +17,16 @@ public class HouseKeeperTest {
         houseKeeper.masterArriveAtHome();
 
         assertTrue(light.isOn());
+    }
+
+    @Test
+    public void should_turn_off_light_when_master_left_home() throws Exception {
+        Light light = new Light();
+        HouseKeeper houseKeeper = new HouseKeeper();
+        houseKeeper.addLight(light);
+        houseKeeper.masterLeftHome();
+
+        assertFalse(light.isOn());
     }
 
 }
